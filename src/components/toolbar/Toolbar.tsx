15709177f3a11e4c794184f9447d3107a3c4efed
ahenteti <ahmad.henteti@gmail.com>
2020-12-26 07:24:00 +0100

    add toolbar icons

--- src/components/toolbar/Toolbar.tsx
+++ src/components/toolbar/Toolbar.tsx
@@ -13,6 +13,23 @@ export default class Toolbar extends React.Component<IToolbarProps, IToolbarStat
   }
 
   public render() {
-    return <div className='toolbar-container'></div>;
+    return (
+      <div className='toolbar-container'>
+        <span className='iconify' data-icon='ic:round-undo' data-inline='false'></span>
+        <span className='iconify' data-icon='ic:round-redo' data-inline='false'></span>
+        <span className='iconify line-separator' data-icon='ant-design:line-outlined' data-inline='false' data-rotate='90deg'></span>
+        <span className='iconify' data-icon='heroicons-outline:zoom-in' data-inline='false'></span>
+        <span className='iconify' data-icon='heroicons-outline:zoom-out' data-inline='false'></span>
+        <span className='iconify line-separator' data-icon='ant-design:line-outlined' data-inline='false' data-rotate='90deg'></span>
+        <span className='iconify' data-icon='ic:twotone-delete-outline' data-inline='false'></span>
+        <span className='iconify line-separator' data-icon='ant-design:line-outlined' data-inline='false' data-rotate='90deg'></span>
+        <span className='iconify to-front-icon' data-icon='whh:bringtofront' data-inline='false'></span>
+        <span className='iconify to-back-icon' data-icon='ri:send-to-back' data-inline='false'></span>
+        <span className='iconify line-separator' data-icon='ant-design:line-outlined' data-inline='false' data-rotate='90deg'></span>
+        <span className='iconify' data-icon='ic:round-format-color-fill' data-inline='false'></span>
+        <span className='iconify' data-icon='ic:round-border-color' data-inline='false'></span>
+        <span className='iconify shadow-icon' data-icon='vaadin:square-shadow' data-inline='false'></span>
+      </div>
+    );
   }
 }
