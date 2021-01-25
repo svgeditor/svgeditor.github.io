72576255d7f4afa0721cf19b4a503854e181c6c8
ahenteti <ahmad.henteti@gmail.com>
2021-01-25 21:28:07 +0100

    add WIP message

--- src/components/navbar/Navbar.tsx
+++ src/components/navbar/Navbar.tsx
@@ -15,12 +15,24 @@ export default class Navbar extends React.Component<INavbarProps, INavbarState> 
   public render() {
     return (
       <div className='navbar-container'>
-        <img className='website-logo' src='/logo.png'></img>
-        <span className='website-name'>SVG Editor</span>
-        <span className='nav-item'>File</span>
-        <span className='nav-item'>Edit</span>
-        <span className='nav-item'>View</span>
-        <span className='nav-item'>Help</span>
+        <div className='navbar-left-container'>
+          <img className='website-logo' src='/logo.png'></img>
+          <span className='website-name'>SVG Editor</span>
+          <span className='nav-item'>File</span>
+          <span className='nav-item'>Edit</span>
+          <span className='nav-item'>View</span>
+          <span className='nav-item'>Help</span>
+        </div>
+        <div className='navbar-right-container'>
+          <span>We are constantly working on adding new features to the website</span>
+          <span>
+            Please feel free to tell us which feature you want to see first by adding new{' '}
+            <a target='_blank' href='https://github.com/svgeditor/svgeditor.github.io/issues'>
+              ticket
+            </a>{' '}
+            on our github repo
+          </span>
+        </div>
       </div>
     );
   }
